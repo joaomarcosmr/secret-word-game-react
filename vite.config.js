2a6cc0e6e53ca1c7ace4bb256ba478cdbb23d544
= <=>
2023-10-08 22:52:10 -0300

    configuracoes de deploy

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/SecretGameWord'
+  base: "/secret-word-game-react",
 })
